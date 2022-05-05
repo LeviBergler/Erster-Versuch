e52a4ebd414974df891dce4e8a15725eb6f6779e
LeviBergler <104757167+LeviBergler@users.noreply.github.com>
2022-05-05 14:15:17 +0200

    Added JS

---
+++ index.js
@@ -0,0 +1,14 @@
+let message = "JS ist toll" // Hi dies ist nicht nötig aber ich habe Lust drauf
+console.log("Hallo von der Konsole")
+
+
+
+function funktion1() {
+    var msg = document.getElementById(`message`).value
+    document.getElementById(`header`).innerText= msg
+    document.getElementById(`message`).value = ""
+}
+
+
+
+// Wieder unnltig aber ich will immernoch diese Nachrichten schreiben
